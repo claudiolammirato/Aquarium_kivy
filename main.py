9d416202283ce31db07f3d677873d8d270c6dd7e
claudiolammirato <claudio.lammirato@gmail.com>
2021-08-31 17:46:39 +0200

    .

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
+from kivy.clock import Clock
+
+import time
+
 
 class MainWidget(BoxLayout):
     pass
 
 class AquariumApp(App):
-    pass
+    def on_start(self):
+        Clock.schedule_interval(self.update_label, 2)
+
+    def update_label(self, *args):
+        print(args)
+
 
 AquariumApp().run()
---
+++ sensors.py
@@ -0,0 +1,5 @@
+class Sensors:
+    port = 0
+
+    def __init__(self, port):
+        self.port = port

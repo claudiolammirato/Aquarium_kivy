1065893bd46192efe4291637febf45fd242756f6
claudiolammirato <claudio.lammirato@gmail.com>
2021-09-02 20:07:47 +0200

    finetuning

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 from graphic import AquariumApp
 from sqlite_database import SQL_Database
+import threading
 
+#INIT SECTION
+#SQlite database Section
+database = SQL_Database('test.db')
+#Table TEMP_INT
+database.create_table("temp_int")
+database.close()
+
+
+#THREADING SECTION
 #Graphic Section
 #AquariumApp().run()
 
-
-#SQlite database Section
-database = SQL_Database('test.db')
-
-database.create_table("temp_int")
-database.close()
+x = threading.Thread(target=AquariumApp().run(), args=(1,))
+x.start()
